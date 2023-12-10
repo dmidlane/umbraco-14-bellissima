098537cb446a48761afddb044eadca0fa894aed7
Dominic Midlane <dominicmidlane@hotmail.com>
2023-12-10 01:06:30 +0000

    add dashboard localisation

---
+++ App_Plugins/welcome-dashboard/localization/da-dk.js
@@ -0,0 +1,7 @@
+export default {
+    welcomeDashboard: {
+      heading: "Velkommen",
+      bodytext: "Dette er Backoffice. Herfra kan du ændre indholdet, medierne og indstillingerne på din hjemmeside.",
+      copyright: "© Sample Selskab 20XX",
+    }
+  };
---
+++ App_Plugins/welcome-dashboard/localization/en-us.js
@@ -0,0 +1,7 @@
+export default {
+    welcomeDashboard: {
+      heading: "Welcome",
+      bodytext: "This is the Backoffice. From here, you can modify the content, media, and settings of your website.",
+      copyright: "© Sample Company 20XX",
+    }
+  };
